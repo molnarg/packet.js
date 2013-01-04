9aeaf4143742794df7c621b284c3227441433936
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-01-04 23:09:10 +0100

    Simplifying fields.field() and deleting the obsolote shift() function.

--- spec/fields.spec.js
+++ spec/fields.spec.js
@@ -40,20 +40,6 @@ function emphasize(string, begin, end, partitions) {
   return string.slice(0, begin) + ' | ' + string.slice(begin, end) + ' | ' + string.slice(end)
 }
 
-function shift(string, offset, begin, end) {
-  if (begin != null || end != null) {
-    return string.slice(0, begin) + shift(string.slice(begin, end), offset) + string.slice(end)
-
-  } else if (offset < 0) {
-    offset = Math.min(-offset, string.length)
-    return string.slice(offset) + Array(offset + 1).join('0')
-
-  } else {
-    offset = Math.min(offset, string.length)
-    return Array(offset + 1).join('0') + string.slice(0, string.length - offset)
-  }
-}
-
 describe("A property ganareted by tools.", function() {
   var buffer = fields.node ? (new Buffer(10)) : (new ArrayBuffer(10))
     , bufferView = new View(buffer)
@@ -135,45 +121,6 @@ describe("A property ganareted by tools.", function() {
         originalState = originalState.substr(0, offset).concat(originalState.substr(offset + t.bitLength))
         expect(newState).toEqual(originalState)
       })
-    })
-  })
-})
-
-describe("fields.shift(view, offset, begin, end)", function() {
-  var buffer = fields.node ? (new Buffer(10)) : (new ArrayBuffer(10))
-    , bufferView = new View(buffer)
-
-  // Generating testcases
-  var tests = [], begin
-  for (var i = 0; i < 100; i++) tests.push({
-    begin: begin = random(buffer.length),
-    end: begin + 1 + random(buffer.length - begin - 1),
-    offset: (random(7) + 1) * ((Math.random() < 0.5) ? 1 : -1)   // -7 - 7 except 0
-  })
-
-  it('should shift the bits of the given view (from bytes begin - end) by offset', function() {
-    tests.forEach(function(t, i) {
-      // Filling the buffer with random bytes
-      for (var i = 0; i < buffer.length; i++) bufferView.writeUInt8(random(256), i)
-
-      // Saving the original state, shifting the data and saving the new state
-      var originalState = bitStringView(bufferView)
-      fields.shift(bufferView, t.offset, t.begin, t.end)
-      var newState = bitStringView(bufferView)
-
-      // Comparing result with string shifting result
-      var bitBegin = t.begin * 8
-        , bitEnd = t.end * 8
-        , shiftedState = shift(originalState, t.offset, bitBegin, bitEnd)
-      expect(newState).toBe(shiftedState)
-
-      if (false)
-      console.log( (shiftedState === newState ? 'successful ' : 'unsuccessful ')
-                 + 'shifting by ' + t.offset + ' from ' + t.begin + ' to ' + t.end + '\n'
-                 + 'original ' + emphasize(originalState, bitBegin, bitEnd, 8) + '\n'
-                 + 'new      ' + emphasize(newState     , bitBegin, bitEnd, 8) + '\n'
-                 + 'expected ' + emphasize(shiftedState , bitBegin, bitEnd, 8) + '\n'
-                 )
     })
   })
 })
