aff45a5e8df0d5d3522c2c4bd630b9a2565ebcf3
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-01-11 23:45:07 +0100

    Adding decoder and printer streams.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
   views: {
     EthernetFrame: require('./lib/views/EthernetFrame')
+  },
+  stream: {
+    decoder: require('./lib/streams/decoder'),
+    printer: require('./lib/streams/printer')
   }
 }
