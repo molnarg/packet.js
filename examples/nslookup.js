2dc47a8b0b34a4692669310178aeaf9300274611
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-02-19 15:20:10 +0100

    Conforming to bt changes

--- examples/nslookup.js
+++ examples/nslookup.js
@@ -1,5 +1,5 @@
 var Packet = require('..')
-  , message = new Packet(1000).dns
+  , message = new Packet(new Buffer(1000)).dns
 message.set({
   id:         1,
   flags:      0x0100,
@@ -14,7 +14,7 @@ message.set({
 })
 
 var socket = require('dgram').createSocket('udp4')
-socket.send(message.root, 0, message.size, 53, '8.8.8.8')
+socket.send(message.buffer, 0, message.size, 53, '8.8.8.8')
 socket.on('message', function(response) {
   Array.prototype.forEach.call(new Packet(response).dns.answers, function(answer) {
     console.log(answer.name.toString(), '->', answer.rdata.toString())
