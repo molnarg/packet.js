f38795458399426cd5e53afc449ff60ce9e40210
Gábor Molnár <gabor.molnar@sch.bme.hu>
2013-02-19 16:20:17 +0100

    DomainName and nslookup bugfix

--- examples/nslookup.js
+++ examples/nslookup.js
@@ -1,6 +1,8 @@
 var Packet = require('..')
-  , message = new Packet(new Buffer(1000)).dns
-message.set({
+  , message = new Packet(new Buffer(1000))
+  , socket = require('dgram').createSocket('udp4')
+
+message.dns = {
   id:         1,
   flags:      0x0100,
   queries:    [{
@@ -11,12 +13,13 @@ message.set({
   answers:    [],
   auth:       [],
   additional: []
-})
+}
 
-var socket = require('dgram').createSocket('udp4')
-socket.send(message.buffer, 0, message.size, 53, '8.8.8.8')
+socket.send(message.buffer, 0, message.dns.size, 53, '8.8.8.8')
+
 socket.on('message', function(response) {
-  Array.prototype.forEach.call(new Packet(response).dns.answers, function(answer) {
+  response = new Packet(response)
+  response.dns.answers.forEach(function(answer) {
     console.log(answer.name.toString(), '->', answer.rdata.toString())
   })
 
